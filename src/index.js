4c6ef0cb1e0d6b793fd893dd8305f6546d342879
ethereal element <vitalik.safonov@list.ru>
2023-08-25 19:34:16 +0300

    init: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const flatArr = bracketsConfig.flat().concat(str);
+
+  let stack = [];
+  let brackets = {
+    ")":"(",
+    "}":"{",
+    "]": "["
+  }
+
+  function isClosedBracket(ch) {
+    return [")", "}", "]"].indexOf(ch) > -1;
+  }
+
+
+  for (let i = 0; i < flatArr.length; i++) {
+    const current = flatArr[i];
+  
+    
+    if (isClosedBracket(current)) {
+      if (brackets[current] !== stack.pop()) return false
+    } else {
+      stack.push(current)
+    }
+  }
+
+  return stack.length === 0;
+
 }
